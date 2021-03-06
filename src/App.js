7f86e96e74d59e1631803ad378ab5bce0f718497
ElzaanVan <75164867+ElzaanVan@users.noreply.github.com>
2021-03-06 15:04:46 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+document.getElementById("loan-form").addEventListener("submit", function (e) {
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 1500);
+
+  e.preventDefault();
+});
+
+function calculateResults() {
+  //UI variables
+  const amount = document.getElementById("amount");
+  const interest = document.getElementById("interest");
+  const years = document.getElementById("years");
+
+  const monthlyPayments = document.getElementById("monthly-payment");
+  const totalPayment = document.getElementById("total-payment");
+  const totalInterest = document.getElementById("total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayments.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    document.getElementById("results").style.display = "block";
+    document.getElementById("loading").style.display = "none";
+  } else {
+    showError("Please check your numbers");
+  }
+}
+
+//Show Error
+function showError(error) {
+  //Create a div
+  const errorDiv = document.createElement("div");
+  //Get elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+  //Add an error class
+  errorDiv.className = "alert alert-danger";
+  //Create a text node
+  errorDiv.appendChild(document.createTextNode(error));
+  //Insert Error above heading
+  card.insertBefore(errorDiv, heading);
+  //No Display of result and loading element
+  document.getElementById("results").style.display = "none";
+  document.getElementById("loading").style.display = "none";
+  //Set timeout
+  setTimeout(clearError, 3000);
+}
+
+function clearError() {
+  document.querySelector(".alert").remove();
+}
